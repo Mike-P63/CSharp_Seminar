bbb85b441e31f955a4cc0f6bf63bb7bb5c0d9755
Mike-P63 <rumaish@rambler.ru>
2022-12-16 17:19:07 +0400

    add Task 6 What number is even?

---
+++ Task_006_Even_Number/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task #6 What number is even?
+
+Console.Clear();
+
+Console.WriteLine("Введите число:");
+            int num = int.Parse(Console.ReadLine()!);
+
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+            }
+            else
+            {
+                Console.WriteLine("Число " + num + " является: ЧЁТНЫМ");
+            }
+
