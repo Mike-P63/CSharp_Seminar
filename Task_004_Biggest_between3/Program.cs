9b9515118bb7cae6c1dc15e1aa8005642494055b
Mike-P63 <rumaish@rambler.ru>
2022-12-16 16:52:29 +0400

    add Task 4 The biggest number between 3

---
+++ Task_004_Biggest_between3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Task #4 The biggest number between 3
+Console.Clear();
+
+Console.WriteLine("Введите 3 числа:");
+            int num_1 = int.Parse(Console.ReadLine()!);
+            int num_2 = int.Parse(Console.ReadLine()!);
+            int num_3 = int.Parse(Console.ReadLine()!);
+
+            int max = num_1;
+
+            if (num_2 > max)
+            {
+                max = num_2;
+            }
+
+            if (num_3 > max)
+            {
+                max = num_3;
+            }
+
+            Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+
