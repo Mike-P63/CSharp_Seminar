976442c673dfe386af9980a9ae25224262a8f7be
Mike-P63 <rumaish@rambler.ru>
2022-12-16 16:40:29 +0400

    add Task 2 What number is the biggest

---
+++ Task_002_Bigest/Program.cs
@@ -0,0 +1,20 @@
+﻿// Task #2 What number is bigger
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int num_A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число: ");
+int num_B = int.Parse(Console.ReadLine()!);
+
+if (num_A > num_B)
+{
+    Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
+}
+else
+{
+    Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+}
+
+
