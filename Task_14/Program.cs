6d01175783ee1975e1b8c56f59d6fddc845dcfc5
Mike-P63 <rumaish@rambler.ru>
2022-12-19 23:18:47 +0400

    Add Task 14 for 7 and 23

---
+++ Task_14/Program.cs
@@ -0,0 +1,19 @@
+﻿//14 Напишите программу, которая принимает на вход число и 
+//проверяет, кратно ли оно одновременно 7 и 23.
+
+void numbers(int firstNumber)
+{
+    int result = firstNumber % 7;
+    int result2 = firstNumber % 23;
+    if (result == 0 && result2 == 0)
+        {
+            Console.WriteLine("Кратно");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+}
+
+int firstNumber = int.Parse(Console.ReadLine()!);
+numbers(firstNumber);
