24a3d5602d61228b9916b81f57b5dc50b2a163a1
Mike-P63 <rumaish@rambler.ru>
2022-12-23 19:40:20 +0400

    Add Task 010 Second Digit

--- Task_010_Second_Digit/Program.cs
+++ Task_010_Second_Digit/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число ");
+int number = int.Parse(Console.ReadLine()!);
+int amount = number.ToString().Length;
+int InCenter(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
