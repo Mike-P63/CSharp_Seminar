b260c942b883addc359b49e107cd7e2e952964ef
Mike-P63 <rumaish@rambler.ru>
2023-01-06 21:11:52 +0400

    Added  task 026 odd and even and sum

---
+++ Task_026_odd_even/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+
+uint oddNumbersCount = 0;
+uint evenNumbersCount = 0;
+
+int oddNumbersSum = 0;
+int evenNumbersSum = 0;
+
+Console.WriteLine("Ведите первое число диапазона");
+int currentValue = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ведите последнее число диапазона");
+int limit = int.Parse(Console.ReadLine());
+
+while (currentValue <= limit)
+{
+    if (currentValue % 2 == 0)
+    {
+        evenNumbersCount++;
+        // evenNumbersSum = evenNumbersSum + currentValue;
+        evenNumbersSum += currentValue;
+    }
+    else
+    {
+        oddNumbersCount++;
+        oddNumbersSum = oddNumbersSum + currentValue;
+    }
+    currentValue++;
+}
+
+Console.WriteLine("Количество нечетных чисел: " + oddNumbersCount);
+Console.WriteLine("Количество четных чисел: " + evenNumbersCount);
+Console.WriteLine("Сумма нечетных чисел: " + oddNumbersSum);
+Console.WriteLine("Сумма четных чисел: " + evenNumbersSum);
+Console.ReadLine();
