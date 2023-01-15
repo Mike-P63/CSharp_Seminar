533e86a2676aacf9ff3b09fca585ce272dbe4314
Mike-P63 <rumaish@rambler.ru>
2023-01-15 20:05:58 +0400

    Added Task 034 Massive Random

---
+++ Task_034_Massive_Random/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, которая покажет 
+количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int size = getUserData($"Введите размер массива ");
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FindEven(int[] array)
+{
+    int even = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        { even = even + 1; }
+    }
+    Console.WriteLine($"всего {numbers.Length} чисел, {even} из них чётные");
+}
+
+FindEven(numbers);
