ff07ab5932bbbd3d6fab24013b0dbe0207b45ace
Mike-P63 <rumaish@rambler.ru>
2023-01-15 16:07:09 +0400

    Added Task 24 from Seminar 12.01.23

---
+++ Task_24_Seminar_12_01/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 24: Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+7 -- 28;
+4 -- 19;
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int getSumOfRange(int start, int end)
+{
+    int sum = 0;
+    for (int i = start; i <= end; i++)
+    {
+        sum += i; //sum = sum + 1;
+    }
+    return sum;
+}
+
+void showData(string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(data);
+    Console.ResetColor();    
+}
+
+int end = getUserData("Введите число А для получения суммы ");
+int sum = getSumOfRange(1, end);
+showData($"Сумма чисел от 1 до {end} =  " ,sum);
