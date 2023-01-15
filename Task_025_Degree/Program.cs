c699d9427469c1a62a5fdcb7da0c60e4799d2ed0
Mike-P63 <rumaish@rambler.ru>
2023-01-15 15:02:27 +0400

    Added changes in task 025 Degree

--- Task_025_Degree/Program.cs
+++ Task_025_Degree/Program.cs
@@ -1,8 +1,9 @@
-﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
-// и возводит число A в натуральную степень B.
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B.
 
-//3, 5 -> 243 (3⁵)
-//2, 4 -> 16
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
 
 Console.Clear();
 
@@ -12,16 +13,16 @@ int Chislo(string message)
     return int.Parse(Console.ReadLine()!);
 }
 
-int number1 = Chislo("Введите число A: ");
-int number2 = Chislo("Введите число B: ");
-Degree(number1, number2);
-
-void Degree(int a, int b)
+void Degree(int number1, int number2)
 {
     int result = 1;
-    for (int i = 1; i <= b; i++)
+    for (int i = 1; i <= number2; i++)
     {
-        result = result * a;
+        result = result * number1;
     }
     Console.WriteLine(result);
 }
+
+int number1 = Chislo("Введите число A: ");
+int number2 = Chislo("Введите число B: ");
+Degree(number1, number2);
