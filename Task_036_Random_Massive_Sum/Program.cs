421e85ae179865c9782fe321519dccf1be80b0fd
Mike-P63 <rumaish@rambler.ru>
2023-01-15 21:32:20 +0400

    Added Task 036 Random Massive_Sum

---
+++ Task_036_Random_Massive_Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+Console.Clear();
+
+int size = 10;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int sumNumbersIndex = 0;
+
+void Sum(int sumNumbersIndex)
+{
+    for (int i = 1; i < numbers.Length; i += 2)
+    {
+        sumNumbersIndex += numbers[i];
+    }
+    Console.WriteLine($"Сумма элементов на нечетных позициях = {sumNumbersIndex}");
+}
+
+Sum(sumNumbersIndex);
+
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
