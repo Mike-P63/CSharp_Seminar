e768e87bae6c595f316c0b71feff358cb4f07817
Mike-P63 <rumaish@rambler.ru>
2023-01-16 19:52:30 +0400

    Added changes in  Task 034 Massive Random

--- Task_034_Massive_Random/Program.cs
+++ Task_034_Massive_Random/Program.cs
@@ -45,7 +45,7 @@ void FindEven(int[] array)
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i] % 2 == 0)
-        { even = even + 1; }
+        even = even + 1; 
     }
     Console.WriteLine($"всего {numbers.Length} чисел, {even} из них чётные");
 }
