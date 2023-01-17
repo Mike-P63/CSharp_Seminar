287d134505293dfe1e09e5c238a123d56e2bcf2b
Mike-P63 <rumaish@rambler.ru>
2023-01-17 10:42:16 +0400

    Added changes at task 33 from Seminar 16 01 23

--- Task_33_Seminar_16_01/Program.cs
+++ Task_33_Seminar_16_01/Program.cs
@@ -10,15 +10,11 @@
 [1, 2, 3, 6, 2] -> 0
 [10, 11, 12, 13, 14] -> 5
 
-Задача 37: Найдите произведение пар чисел в одномерном массиве. 
-Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
-Результат запишите в новом массиве.
-[1 2 3 4 5] -> 5 8 3
-[6 7 3 6] -> 36 21
 */
 
 Console.Clear();
 
+/*
 
 int[] generateArray(int length)
 {
@@ -69,9 +65,74 @@ int Number = getUserNumber("Введите проверочное число: ")
 int Index = LookForNumber(Array, Number);
 if (Index >=0)
 {
-    Console.WriteLine($"Искомое число находится на позиции с индексом {Index + 1}");
+    Console.WriteLine($"Искомое число находится на позиции {Index + 1}");
 }
     else
     {
         Console.WriteLine("Такого числа нет ");
     }
+*/
+
+/*Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21
+*/
+
+Console.Clear();
+
+int[] Fillarray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(start, end +1);
+    }
+    return array;
+}
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+int[] Newarray(int[] array)
+{
+    int newlength = (int)Math.Ceiling(array.Length / 2.0);
+    int[] arr = new int[newlength];
+    for (int i = 0; i < newlength; i++)
+    {
+        if (i < newlength - 1 || array.Length%2==0)
+        {
+            arr[i] = array[i] * array[array.Length- i-1];
+        }
+        else
+        {
+              arr[i] = array[i];      
+        }
+    } 
+    return arr;
+    
+}
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length -1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int number = getUserData("Введите количество элементов массива");
+int[] array = Fillarray(number, -10, 10);
+printArray(array);
+int[] array2 = Newarray(array);
+printArray(array2);
+
+
