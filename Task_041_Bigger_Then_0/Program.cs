9e4fc9e2e91b4c37057e6f9c4df70673f85fef40
Mike-P63 <rumaish@rambler.ru>
2023-01-17 21:38:31 +0400

    Added task 041 Bigger then 0

---
+++ Task_041_Bigger_Then_0/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int ArrayLen = getUserData("Введите количество элементов массива \t ");
+
+void FillArray()
+{
+    int[] Array = new int[ArrayLen];
+    int sum = 0;
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t");
+        Array[i] = int.Parse(Console.ReadLine());
+    }
+    Console.Write("[ ");
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i] + " ");
+    }
+    Console.WriteLine(" ]");
+    
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > 0)
+        {
+        sum = sum + 1;
+        }
+    }
+    Console.Write($"Чисел, значение которых больше чем 0: -> {sum}");
+}
+FillArray();
+
+ 
