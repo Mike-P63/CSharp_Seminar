b508667b2c8c264952f7ab09418cf6618247d136
Mike-P63 <rumaish@rambler.ru>
2023-01-22 15:54:53 +0400

    Added Task 45 Copy Massive

---
+++ Task_45_Copy_Massive/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Задача 45: Напишите программу, которая будет создавать копию заданного 
+массива с помощью поэлементного копирования.
+*/ 
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int[] FillArrayRandomNumbers(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(start, end +1);
+    }
+    return array;
+}
+int[] CopyArray(int[] array)
+{
+    int[] result = new int [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[i];
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ,");
+    }
+    Console.WriteLine("]");
+}
+
+int number = getUserData($"Введите размер массива ");
+int[] array = FillArrayRandomNumbers(number, -10,10);
+PrintArray(array);
+int[] result = CopyArray(array);
+PrintArray(result);
