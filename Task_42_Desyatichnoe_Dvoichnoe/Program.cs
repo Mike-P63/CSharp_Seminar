201438c1a703655bc59a512eaede920aee53b4c4
Mike-P63 <rumaish@rambler.ru>
2023-01-22 13:18:38 +0400

    Added Task 42 Desyatichnoe v Dvoichnoe

---
+++ Task_42_Desyatichnoe_Dvoichnoe/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 42: Напишите программу, которая будет преобразовывать 
+десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10.
+*/ 
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int UserData = int.Parse(Console.ReadLine()!);
+    return UserData;
+}
+
+
+string TransformBinary(int number)
+{
+    string result = string.Empty;
+    while (number>0)
+    {
+        result=number%2+result;
+        number=number/2;
+    }
+    return result;
+}
+int number = getUserData("Введите десятичное число ");
+string binary = TransformBinary(number);
+Console.WriteLine($"Число {number} в двоичной системе равно {binary}");
