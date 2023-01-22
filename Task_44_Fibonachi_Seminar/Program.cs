fa42840ee3f19176c01c8856279d2f39eff7c95d
Mike-P63 <rumaish@rambler.ru>
2023-01-22 17:53:27 +0400

    Added Task 44 Fibonachi Seminar

---
+++ Task_44_Fibonachi_Seminar/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+*/ 
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int UserData = int.Parse(Console.ReadLine()!);
+    return UserData;
+}
+
+string Fionachi(int number)
+{
+    string result = string.Empty;
+    int n1 = 0;
+    int n2 = 1;
+    result = $"{n1}{n2}";
+    for (int i = 2; i < number; i++)
+    {
+        int n = n1 + n2;
+        result = result + n;
+        n1 = n2;
+        n2 = n;
+    }
+    return result;
+}
+int number = getUserData("n");
+string result = Fionachi(number);
+Console.WriteLine(result);
