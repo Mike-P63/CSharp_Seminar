d17439b560b088e8a2586217d9439bbdd8a51e29
Mike-P63 <rumaish@rambler.ru>
2023-01-23 23:30:56 +0400

    Added task 46 Seminar Massive Drobi

---
+++ Task_46_Seminar_Massive_Drobi/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 46
+
+int getDataFromUser(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+void printInColor(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+double[,] get2DDoubleArray(int colLength, int rowLength, int start, int finish)
+{
+    double[,] array = new double[colLength, rowLength];
+    //int divider = 100;
+    for (int i = 0; i < colLength; i++)
+    {
+        for (int j = 0; j < rowLength; j++)
+        {
+            //array[i,j] = new Random().Next(start * divider , (finish+1) * divider)/(divider + 0.0);
+            array[i,j] = new Random().Next(start, finish +1) + Math.Round(new Random().NextDouble(),2);
+        }
+    }
+    return array;
+}
+
+void print2DArray(double[,] array)
+{
+    Console.Write("\t");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        printInColor(j + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+            printInColor(i + "\t");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i,j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+}
+
+int n = getDataFromUser("Введите количество строк");
+int m = getDataFromUser("Введите количество столбцов");
+double[,] array = get2DDoubleArray(n,m,0,100);
+print2DArray(array);
