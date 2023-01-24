405d3bc522bf4135d66d2e501d8fc5ad57a5d88a
Mike-P63 <rumaish@rambler.ru>
2023-01-24 15:27:16 +0400

    Added changes at task 047 Double Massive Random

--- Task_047_Double_MassiveRandom/Program.cs
+++ Task_047_Double_MassiveRandom/Program.cs
@@ -19,10 +19,17 @@ int getUserData(string message)
     int userData = int.Parse(Console.ReadLine()!);
     return userData;
 }
-int lines = getUserData("Введите количество строк двумерного массива m = ");
+int rows = getUserData("Введите количество строк двумерного массива m = ");
 int columns = getUserData("Введите количество столбцов двумерного массива n = ");
 
-double[,] numbers = new double[lines, columns];
+void printInColor(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+double[,] numbers = new double[rows, columns];
 
 void FillArrayRandomNumbers(double[,] array)
 {
@@ -30,19 +37,26 @@ void FillArrayRandomNumbers(double[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = Convert.ToDouble(new Random().Next(-10, 10));
+            //array[i, j] = Convert.ToDouble(new Random().Next(-100, 100));
+            array[i,j] = new Random().Next(-100, 100) + Math.Round(new Random().NextDouble(),2);
         }
     }
 }
 
 void PrintArray(double[,] array)
 {
+    Console.Write("\t");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        printInColor(j + "\t");
+    }
+    Console.WriteLine();
     for (int i = 0; i < array.GetLength(0); i++)
     {
-
+        printInColor(i + "\t");
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write(array[i, j] + " ");
+            Console.Write(array[i, j] + "\t");
         }
         Console.WriteLine(" ");
     }
