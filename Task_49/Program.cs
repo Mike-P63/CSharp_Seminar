5ec66ec40effe121327150192c2621994cc61cce
Mike-P63 <rumaish@rambler.ru>
2023-01-24 17:02:27 +0400

    Added task 49 Double array even square

---
+++ Task_49/Program.cs
@@ -0,0 +1,85 @@
+﻿/*
+Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба 
+индекса чётные, и замените эти элементы на их квадраты.
+Например, изначально массив
+ выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+*/
+
+Console.Clear();
+
+int getDataFromUser(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+void printInColor(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+int[,] AddArray(int colLength, int rowLength, int start, int end)
+{
+    int[,] array = new int[colLength, rowLength];
+    for (int i = 0; i < colLength; i++)
+    {
+        for (int j = 0; j < rowLength; j++)
+        {
+            array[i, j] = new Random().Next(start, end + 1);
+        }
+    }
+    return array;
+}
+
+void printArray(int[,] array)
+{
+    Console.Write("\t");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        printInColor(j + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] Change(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = (int)Math.Pow(array[i, j], 2);
+            }
+        }
+    }
+    return array;
+}
+
+
+int colLength = getDataFromUser("Введите количество строк");
+int rowLength = getDataFromUser("Введите количество столбцов");
+int[,] array = AddArray(colLength, rowLength, 1, 10);
+printArray(array);
+Console.WriteLine();
+int[,] arraySquare = Change(array);
+printArray(arraySquare);
+
