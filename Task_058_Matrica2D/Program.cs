c67b625f141f4dfd0664c8ce4ef8da1df5f3b387
Mike-P63 <rumaish@rambler.ru>
2023-01-29 12:38:59 +0400

    Added Task 058 Matrica 2D

---
+++ Task_058_Matrica2D/Program.cs
@@ -0,0 +1,87 @@
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, 
+которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/ 
+
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int rows = getUserData("Введите количество строк матрицы: ");
+int columns = getUserData("Введите количество столбцов матрицы: ");
+int[,] array = new int[rows, columns];
+int[,] secondArray = new int[rows, columns];
+int[,] resultArray = new int[rows, columns];
+
+FillArrayRandomNumbers(array);
+PrintArray2D(array);
+
+Console.WriteLine();
+
+FillArrayRandomNumbers(secondArray);
+PrintArray2D(secondArray);
+
+Console.WriteLine();
+
+Matrice(array);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToInt32(new Random().Next(1, 10));
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Matrice(int [,] array)
+{
+if (array.GetLength(0) != secondArray.GetLength(1))
+{
+    Console.WriteLine(" Данную матрицу нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
+    {
+        resultArray[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            resultArray[i, j] += array[i, k] * secondArray[k, j];
+        }
+    }
+}
+PrintArray2D(resultArray);
+}
+
+
+
+
