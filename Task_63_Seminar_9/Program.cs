c9c8978aec3f0a9d196259a9a3e8b2e3a92df989
Mike-P63 <rumaish@rambler.ru>
2023-01-30 22:58:11 +0400

    Added Task 63 Seminar 9

---
+++ Task_63_Seminar_9/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 63: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+
+*/ 
+Console.Clear();
+
+int getFromUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result  = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int userNumber = getFromUserData("Введите значение границы ряда");
+
+void printRange(int N)
+{
+    if(N == 0)
+    {
+        return;
+    }
+    printRange(N - 1);
+    Console.Write(N + " , ");
+}
+printRange(userNumber);
+Console.WriteLine();
