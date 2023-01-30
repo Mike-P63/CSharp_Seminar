a943ab1121aa535a56f6b45f3ad1ebafea7e9791
Mike-P63 <rumaish@rambler.ru>
2023-01-30 22:54:11 +0400

    Added Task 67 Summa Digits Recursiya

---
+++ Task_67_Seminar_9/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 67: Напишите программу, которая будет 
+принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/ 
+
+Console.Clear();
+
+int getFromUserData(string msg)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(msg);
+    Console.ResetColor();
+    int message  = int.Parse(Console.ReadLine()!);
+    return message;
+}
+
+int printSum(int number)
+{
+    if(number == 0)
+    {
+        return 0;
+    }
+    return number % 10 + printSum(number / 10);
+}
+
+int num = getFromUserData("Введите число");
+int result = printSum(num);
+Console.WriteLine(result);
