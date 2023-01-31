141757310dca586de8e903faea660d119b977219
Mike-P63 <rumaish@rambler.ru>
2023-01-31 22:27:07 +0400

    Added task 70 Factorial Recursiya

---
+++ Task_70_Factorial_Recursiya/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Факториал числа с помощью рекурсии:
+*/
+
+Console.Clear();
+
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+Console.WriteLine();
