c756a6892f99f0e249e488743593f687738fbfe9
Mike-P63 <rumaish@rambler.ru>
2023-01-31 11:39:19 +0400

    Added Task 064 Print Range Recurciya

---
+++ Task_064_Print_Range_Recur/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке 
+от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+
+Console.Clear();
+
+int getDataFromUser(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int userData = getDataFromUser("Введите значение границы ряда");
+
+void printRange(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    Console.Write(N + " , ");
+    printRange(N - 1);
+}
+Console.WriteLine();
+printRange(userData);
+Console.WriteLine();
